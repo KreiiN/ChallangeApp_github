7a61b53ab90e6c1c83adfcdc5c7cffb12aa87f51
KreiiN <kamil290909@gmail.com>
2024-12-10 19:21:08 +0100

    Zadanie domowe, dzien 4

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+string plec = "K";
+var Age = 30;
+
+
+if (plec == "K" && Age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && Age == 30)
+{
+    Console.WriteLine("Ewa lat 30");
+}
+else if (plec == "M" && Age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine("Nie pasuje do programu");
+}
