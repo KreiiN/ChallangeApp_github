a700a92abc041fe3f77c1418049b88c4b7115554
KreiiN <kamil290909@gmail.com>
2024-12-11 21:11:28 +0100

    zadanie domowe  dzien 5

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,21 +1,69 @@
-﻿var name = "Ewa";
-string plec = "K";
-var Age = 30;
+﻿int number = 4566;
+string numberAsString = number.ToString();
+char[] signs = numberAsString.ToArray();
 
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
+{
+    foreach (char sign in signs)
 
-if (plec == "K" && Age < 30)
-{
-    Console.WriteLine("Kobieta poniżej 30 lat");
+        if (sign == '0')
+        {
+            counter0++;
+        }
+        else if (sign == '1')
+        {
+            counter1++;
+        }
+        else if (sign == '2')
+        {
+            counter2++;
+        }
+        else if (sign == '3')
+        {
+            counter3++;
+        }
+        else if (sign == '4')
+        {
+            counter4++;
+        }
+        else if (sign == '5')
+        {
+            counter5++;
+        }
+        else if (sign == '6')
+        {
+            counter6++;
+        }
+        else if (sign == '7')
+        {
+            counter7++;
+        }
+        else if (sign == '8')
+        {
+            counter8++;
+        }
+        else if (sign == '9')
+        {
+            counter9++;
+        }
 }
-else if (name == "Ewa" && Age == 30)
-{
-    Console.WriteLine("Ewa lat 30");
-}
-else if (plec == "M" && Age < 18)
-{
-    Console.WriteLine("Niepełnoletni mężczyzna");
-}
-else
-{
-    Console.WriteLine("Nie pasuje do programu");
-}
+Console.WriteLine("*0 ==>" + counter0);
+Console.WriteLine("*1 ==>" + counter1);
+Console.WriteLine("*2 ==>" + counter2);
+Console.WriteLine("*3 ==>" + counter3);
+Console.WriteLine("*3 ==>" + counter3);
+Console.WriteLine("*4 ==>" + counter4);
+Console.WriteLine("*5 ==>" + counter5);
+Console.WriteLine("*6 ==>" + counter6);
+Console.WriteLine("*7 ==>" + counter7);
+Console.WriteLine("*8 ==>" + counter8);
+Console.WriteLine("*9 ==>" + counter9);
